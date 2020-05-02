9bda2f21a0e6705b85a14d838a793ebb733d5b16
Ivano <i.dibenedetto9@studenti.uniba.it>
2020-05-03 00:07:29 +0200

    Context null reference fix.

--- simplepopup/src/main/java/com/ivanodibenedetto/simplepopup/Popup.java
+++ simplepopup/src/main/java/com/ivanodibenedetto/simplepopup/Popup.java
@@ -1,18 +1,23 @@
 package com.ivanodibenedetto.simplepopup;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
 public class Popup extends Activity {
     private String title;
     private String message;
     private String buttonText;
+    private Context context;
 
-    public Popup(String title, String message){
+    public Popup(Context context, String title, String message){
+        this.context = context;
         this.title = title;
         this.message = message;
     }
-    public Popup(String title, String message, String buttonText){
+    public Popup(Context context,String title, String message, String buttonText){
+
+        this.context = context;
         this.title = title;
         this.message = message;
         this.buttonText = buttonText;
@@ -20,7 +25,7 @@ public class Popup extends Activity {
 
     public void Create(){
 
-        Intent i = new Intent (getApplicationContext(), PopupActivity.class);
+        Intent i = new Intent (context, PopupActivity.class);
         i.putExtra("title",title);
         i.putExtra("body",message);
         if(buttonText!=null)
