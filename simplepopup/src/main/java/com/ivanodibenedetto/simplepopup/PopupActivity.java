e35a4b550b2e0eaa1511d37725f89db5e3d27f94
Ivano <i.dibenedetto9@studenti.uniba.it>
2020-05-03 00:38:06 +0200

    Fixed layout

--- simplepopup/src/main/java/com/ivanodibenedetto/simplepopup/PopupActivity.java
+++ simplepopup/src/main/java/com/ivanodibenedetto/simplepopup/PopupActivity.java
@@ -23,7 +23,7 @@ public class PopupActivity extends AppCompatActivity {
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*0.8f),(int)(height*0.6f));
+        //getWindow().setLayout((int)(width*0.8f),(int)(height*0.6f));
 
         Bundle b = getIntent().getExtras();
         if(b!=null) {
